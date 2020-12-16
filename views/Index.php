2cb0e1820170832bffa5ff477872f35a441b3835
prasetyomuhdwi <muhammad.2365@students.amikom.ac.id>
2020-12-16 18:21:34 +0700

    change .htaccess

--- views/Index.php
+++ views/Index.php
@@ -9,7 +9,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="Cache-control" content="no-cache">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Landing Page</title>
+  <title><?php echo $data['title'] ?></title>
 </head>
 
 <body>
